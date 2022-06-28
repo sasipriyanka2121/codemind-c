9ba63fcd1e3c94365c16c967541cc587f4c699ab
sasipriyanka <98680826+sasipriyanka2121@users.noreply.github.com>
2022-06-28 21:24:43 +0530

    #include<stdio.h> int main() {     int n,i,sum=0,arr[100],k=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]>arr[i+1])         {             k++;         }     }     if(k==n)     {         printf("yes");     }     else     {         printf("no");     } }

---
+++ Descending_array.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,sum=0,arr[100],k=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]>arr[i+1])
+        {
+            k++;
+        }
+    }
+    if(k==n)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+}
