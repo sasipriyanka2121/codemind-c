e90fd330a7fe05fd062abe11e16e28af69505a50
sasipriyanka <98680826+sasipriyanka2121@users.noreply.github.com>
2022-06-30 22:09:27 +0530

    #include<stdio.h> int non_prime(int *arr,int p) {     int i,c=0,j,k=0,h;     for(i=0;i<p;i++)     {         for(j=1;j<=arr[i];j++)         {             if(arr[i]%j==0)             {                 c++;             }         }         if(c==2)         {             k++;         }         c=0;     }     h=p-k;     return h; } int main() {     int n,i,arr[100],p=0,h,k;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         if(n%i==0)         {             p++;             arr[k]=i;             k++;         }     }     h=non_prime(arr,p);     printf("%d ",h); }

--- Count_of_the_non-prime_divisors_of_a_given_number.c
+++ Count_of_the_non-prime_divisors_of_a_given_number.c
@@ -1,27 +1,38 @@
 #include<stdio.h>
+int non_prime(int *arr,int p)
+{
+    int i,c=0,j,k=0,h;
+    for(i=0;i<p;i++)
+    {
+        for(j=1;j<=arr[i];j++)
+        {
+            if(arr[i]%j==0)
+            {
+                c++;
+            }
+        }
+        if(c==2)
+        {
+            k++;
+        }
+        c=0;
+    }
+    h=p-k;
+    return h;
+}
 int main()
 {
-	int N,i=1,count=0;
-	scanf("%d",&N);
-	for(i;i<=N;i++)
-	{
-		int sum=0;
-		if(N%i==0)
-		{
-			int j=1;
-			while(j<=i)
-			{
-				if(i%j==0)
-				{
-					sum++;
-				}
-				j++;
-			}
-			if(sum!=2)
-			{
-				count++;
-			}
-		}
-	}
-	printf("%d",count);
+    int n,i,arr[100],p=0,h,k;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            p++;
+            arr[k]=i;
+            k++;
+        }
+    }
+    h=non_prime(arr,p);
+    printf("%d ",h);
 }
