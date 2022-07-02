78ccdcf39b150069641f0fc9aa416ef232d9b2be
sasipriyanka <98680826+sasipriyanka2121@users.noreply.github.com>
2022-07-02 19:56:54 +0530

    #include<stdio.h> int main() {     int n,i,j,a[100],sum=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         for(j=0;j<n;j++)         {             if(a[i]==a[j]&&i!=j)             {                 a[j]=1000;             }         }         if(a[i]!=1000)         {             if(a[i]%2==0)             {                 sum++;             }         }     }     printf("%d",sum); }

---
+++ unique_even_count.c
@@ -0,0 +1,29 @@
+
+#include<stdio.h>
+int main()
+{
+    int n,i,j,a[100],sum=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            if(a[i]==a[j]&&i!=j)
+            {
+                a[j]=1000;
+            }
+        }
+        if(a[i]!=1000)
+        {
+            if(a[i]%2==0)
+            {
+                sum++;
+            }
+        }
+    }
+    printf("%d",sum);
+}
