f21d7a6d8f2437d3fa896f795e680d9032926aab
sasipriyanka <98680826+sasipriyanka2121@users.noreply.github.com>
2022-07-02 19:53:57 +0530

    #include<stdio.h> int main() {     int n,a[100],i,j,c,d=0,b[100],sum=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {   //      printf("%d ",a[i]);     }
        for(i=0;i<n;i++)
        { c=1;
            for(j=0;j<n;j++)
            {
                if(i!=j)
                {
                    if(a[i]==a[j])
                    {  a[j]=1000;
                        c++;
                    }
                }
            }
            if(a[i]!=1000)
            { 
                printf("%d %d ",a[i],c);
            }
        }
    
        //   printf("%d",sum);
        
    
     
    }

---
+++ Element_and_Count.c
@@ -0,0 +1,38 @@
+
+#include<stdio.h>
+int main()
+{
+    int n,a[100],i,j,c,d=0,b[100],sum=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+  //      printf("%d ",a[i]);
+    }
+    
+    for(i=0;i<n;i++)
+    { c=1;
+        for(j=0;j<n;j++)
+        {
+            if(i!=j)
+            {
+                if(a[i]==a[j])
+                {  a[j]=1000;
+                    c++;
+                }
+            }
+        }
+        if(a[i]!=1000)
+        { 
+            printf("%d %d ",a[i],c);
+        }
+    }
+
+    //   printf("%d",sum);
+    
+
+ 
+}
