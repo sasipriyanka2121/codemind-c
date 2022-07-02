90ea066388f397a1d35a08fdb8f9ebd2d8517752
sasipriyanka <98680826+sasipriyanka2121@users.noreply.github.com>
2022-07-02 21:11:37 +0530

    #include<stdio.h> int main() {     int m,i,d=0,n,s=0,t,c=0;     scanf("%d",&m);     scanf("%d",&n);     for(i=m;i<=n;i++)     {         c=0;      t=i;      while(t)      {         d=t%10;         if((d==0)||(i%d!=0))         {             c++;             }             t=t/10;         }         if(c==0)         {             printf("%d ",i);         }     } }

---
+++ Self_Dividing_Numbers.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int m,i,d=0,n,s=0,t,c=0;
+    scanf("%d",&m);
+    scanf("%d",&n);
+    for(i=m;i<=n;i++)
+    {
+        c=0;
+     t=i;
+     while(t)
+     {
+        d=t%10;
+        if((d==0)||(i%d!=0))
+        {
+            c++;
+            }
+            t=t/10;
+        }
+        if(c==0)
+        {
+            printf("%d ",i);
+        }
+    }
+}
