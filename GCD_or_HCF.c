a8c322b077a85c272c8a28a01f81a74b936b21de
sasipriyanka <98680826+sasipriyanka2121@users.noreply.github.com>
2022-07-02 22:50:03 +0530

    #include<stdio.h> int findgcd(int a,int b) {     int res;     while(b)     {         int res;         res=a%b;         a=b;         b=res;     }     return a; } int main() { int a,b,gcd; scanf("%d%d",&a,&b); gcd=findgcd(a,b); printf("%d",gcd); return 0; }

--- GCD_or_HCF.c
+++ GCD_or_HCF.c
@@ -1,22 +1,21 @@
 #include<stdio.h>
+int findgcd(int a,int b)
+{
+    int res;
+    while(b)
+    {
+        int res;
+        res=a%b;
+        a=b;
+        b=res;
+    }
+    return a;
+}
 int main()
 {
-    int n,m,i,t;
-    scanf("%d%d",&n,&m);
-    if(n<m)
-    {
-        t=n;
-    }
-    else
-    {
-        t=m;
-    }
-    for(i=t;i>=1;i--)
-    {
-        if(n%i==0 && m%i==0)
-        {
-            printf("%d",i);
-            break;
-        }
-    }
+int a,b,gcd;
+scanf("%d%d",&a,&b);
+gcd=findgcd(a,b);
+printf("%d",gcd);
+return 0;
 }
