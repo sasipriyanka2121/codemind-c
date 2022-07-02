bbc0312df0d741eb5f7e4e37df193480e9e4d405
sasipriyanka <98680826+sasipriyanka2121@users.noreply.github.com>
2022-07-02 19:38:02 +0530

    #include<stdio.h> int main() {     int n,a[100],i,v=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=n-1;i>=0;i--)     {         if(a[i]%2==0)         {             v=a[i];             break;         }     }     printf("%d",v); }

---
+++ Last_even_number_in_an_array.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[100],i,v=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=n-1;i>=0;i--)
+    {
+        if(a[i]%2==0)
+        {
+            v=a[i];
+            break;
+        }
+    }
+    printf("%d",v);
+}
