54fcad663c8c5bf5b94dc3b8aaeed36c0fdfaa24
sasipriyanka <98680826+sasipriyanka2121@users.noreply.github.com>
2022-07-12 22:42:23 +0530

    #include<stdio.h> int main() {     int n,i,a[100],c,d,t,k=1,s=0,x=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<=n-1;i++)     {         c=0;         t=a[i];         if(a[i]==0)         {             c=1;         }         while(t!=0)         {             c++;             d=t%10;             t=t/10;         }         if(k<c)         {             k=c;         }     }     for(i=0;i<=n-1;i++)     {         c=0;         t=a[i];         if(a[i]==0)         {             c=1;         }         while(t!=0)         {             c++;             d=t%10;             t=t/10;         }         if(c==k)         {             x++;//printf("%d ",a[i]);         }
        }
        printf("%d",x);
    }

--- Max_digits_count.c
+++ Max_digits_count.c
@@ -5,46 +5,46 @@ int main()
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
-      scanf("%d",&a[i]);
+        scanf("%d",&a[i]);
     }
-        for(i=0;i<=n-1;i++)
+    for(i=0;i<=n-1;i++)
+    {
+        c=0;
+        t=a[i];
+        if(a[i]==0)
         {
-            c=0;
-            t=a[i];
-            if(a[i]==0)
-            {
-                c=1;
-            }
-            while(t!=0)
-            {
-                c++;
-                d=t%10;
-                t=t/10;
-            }
-            if(k<c)
-            {
-                k=c;
-            }
-        }
-        for(i=0;i<=n-1;i++)
+            c=1;
+        }        
+        while(t!=0)
         {
-            c=0;
-            t=a[i];
-            if(a[i]==0)
-            {
-                c=1;
-            }
-            while(t!=0)
-            {
-                c++;
-                d=t%10;
-                t=t/10;
-            }
-            if(c==k)
-            {
-               x++;//print("%d",a[i]);
-            }
-            
+            c++;
+            d=t%10;
+            t=t/10;
         }
-        printf("%d",x);
+        if(k<c)
+        {
+            k=c;
+        }
     }
+    for(i=0;i<=n-1;i++)
+    {
+        c=0;
+        t=a[i];
+        if(a[i]==0)
+        {
+            c=1;
+        }        
+        while(t!=0)
+        {
+            c++;
+            d=t%10;
+            t=t/10;
+        }
+        if(c==k)
+        {
+            x++;//printf("%d ",a[i]);
+        }
+        
+    }
+    printf("%d",x);
+}
