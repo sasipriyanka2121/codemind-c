980512af7bca01a60822ef2ade2681cc2a4bebd0
sasipriyanka <98680826+sasipriyanka2121@users.noreply.github.com>
2022-07-13 23:20:13 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);         printf("%d ",a[i]);     }     if(n%2!=0)     {         printf("0");     } }

--- Elements_with_pairs.c
+++ Elements_with_pairs.c
@@ -1,33 +1,16 @@
-
 #include<stdio.h>
 int main()
 {
-    int n,a[100],i,j,c,d=0,b[100],sum=0;
+    int n;
     scanf("%d",&n);
+    int a[n],i;
     for(i=0;i<n;i++)
     {
         scanf("%d",&a[i]);
+        printf("%d ",a[i]);
     }
     if(n%2!=0)
     {
-      for(i=0;i<=n;i++)
-    {     if(i==n)
-         {
-             printf("0");
-         }
-         else
-         {
-          printf("%d ",a[i]);
-         }
+        printf("0");
     }
-    }//   printf("%d",sum);
-    else
-    {
-       for(i=0;i<n;i++)
-    {
-        printf("%d ",a[i]);
-    }   
-    }
-
- 
 }
